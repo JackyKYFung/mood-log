7e9203147060159239dd064d23ee7a305f4533f6
Jacky Fung <jackyfung@Jackys-MacBook-Pro.local>
2026-03-13 11:01:22 -0700

    react app setup + css  cleanup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+return (
+  <div>
+    <h1>mood</h1>
+    <button>sleepy</button>
+    <button>energized</button>
+    <button>tired</button>
+  </div>
+)
+
+}
+
+export default App
